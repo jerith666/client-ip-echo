aa9563c76f83bbda5cbc8d67585cf63bfc90b06a
Matt McHenry <github@matt.mchenryfamily.org>
2016-08-22 21:59:44 -0400

    listen on all addrs

--- client-ip-echo.hs
+++ client-ip-echo.hs
@@ -3,17 +3,9 @@
 import Network
 import Network.Socket
 
---- we don't have 2.6.3.0 :(
---- localhost = tupleToHostAddress (fromInteger 127,
----                                fromInteger 0,
----				fromInteger 0,
----				fromInteger 1)
---- localhost = ntohl (fromInteger 127 * 2^24 + 1)
-localhost = 127 + 1 * 2^24 ---little-endian only :-/
-
 main = do {
          ios <- socket AF_INET Stream defaultProtocol;
-	 bind ios (SockAddrInet (4242::PortNumber) localhost );
+	 bind ios (SockAddrInet (4242::PortNumber) iNADDR_ANY );
 	 listen ios 1;
 	 (s, addr) <- Network.Socket.accept ios;
 	 putStrLn "accepted connection";
