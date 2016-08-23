be0229e67e98df65dece72b569186699371d317a
Matt McHenry <github@matt.mchenryfamily.org>
2016-08-22 22:29:05 -0400

    attempt to show ip of other side

--- client-ip-echo.hs
+++ client-ip-echo.hs
@@ -2,11 +2,26 @@
 
 --- import Network
 import Network.Socket
+import Data.Bits
+import Data.Word
 
 main = do {
          ios <- socket AF_INET Stream defaultProtocol;
 	 bind ios (SockAddrInet (4242::PortNumber) iNADDR_ANY );
 	 listen ios 1;
 	 (s, addr) <- accept ios;
-	 putStrLn "accepted connection";
+	 putStrLn ("accepted connection from " ++ (hostaddr addr));
        }
+
+shiftmask :: Word32 -> Int -> Integer -> Integer
+shiftmask x y z = (.&.) (shiftR (toInteger x) y) z
+
+hostaddr :: SockAddr -> String
+hostaddr (SockAddrInet _ hAddr) =
+  let 
+      w1 = shiftmask hAddr 24 8
+      w2 = shiftmask hAddr 16 8
+      w3 = shiftmask hAddr 8  8
+      w4 = shiftmask hAddr 0  8
+  in (show w1) ++ "." ++ (show w2) ++ "." ++ (show w3) ++ "." ++ (show w4)
+hostaddr _ = "unsupported address type"
