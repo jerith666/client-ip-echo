4ccde6959d20f68aa401ef590e2b8264c8f73d7d
Matt McHenry <github@matt.mchenryfamily.org>
2016-08-22 21:58:21 -0400

    fix localhost addr

--- client-ip-echo.hs
+++ client-ip-echo.hs
@@ -3,11 +3,17 @@
 import Network
 import Network.Socket
 
-localhost = 127 * 2^24 + 1
+--- we don't have 2.6.3.0 :(
+--- localhost = tupleToHostAddress (fromInteger 127,
+---                                fromInteger 0,
+---				fromInteger 0,
+---				fromInteger 1)
+--- localhost = ntohl (fromInteger 127 * 2^24 + 1)
+localhost = 127 + 1 * 2^24 ---little-endian only :-/
 
 main = do {
          ios <- socket AF_INET Stream defaultProtocol;
-	 bind ios (SockAddrInet (4242::PortNumber) (fromInteger localhost) );
+	 bind ios (SockAddrInet (4242::PortNumber) localhost );
 	 listen ios 1;
 	 (s, addr) <- Network.Socket.accept ios;
 	 putStrLn "accepted connection";
