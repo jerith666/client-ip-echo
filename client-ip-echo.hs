3d092f6e6b497b5db5ba119f86fe2509bd21eb9e
Matt McHenry <github@matt.mchenryfamily.org>
2016-08-22 22:01:05 -0400

    simplify accept by removing unnecessary import

--- client-ip-echo.hs
+++ client-ip-echo.hs
@@ -1,12 +1,12 @@
 --- nix-shell -p "haskellPackages.ghcWithPackages (pkgs: [pkgs.network])"
 
-import Network
+--- import Network
 import Network.Socket
 
 main = do {
          ios <- socket AF_INET Stream defaultProtocol;
 	 bind ios (SockAddrInet (4242::PortNumber) iNADDR_ANY );
 	 listen ios 1;
-	 (s, addr) <- Network.Socket.accept ios;
+	 (s, addr) <- accept ios;
 	 putStrLn "accepted connection";
        }
